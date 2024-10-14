748b2bc796bf37797ac69698b01fcd12e8972647
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-15 02:44:38 +0800

    Add constructor, setter and getter methods for attributes in "Car" class

--- src/main/java/seedu/Car.java
+++ src/main/java/seedu/Car.java
@@ -2,4 +2,37 @@ package seedu;
 
 public class Car {
 
+    private String model;
+    private String licensePlateNumber;
+    private double price;
+
+    public Car(String model, String licensePlateNumber, double price) {
+        this.model = model;
+        this.licensePlateNumber = licensePlateNumber;
+        this.price = price;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setLicensePlateNumber(String licensePlateNumber) {
+        this.licensePlateNumber = licensePlateNumber;
+    }
+
+    public String getLicensePlateNumber() {
+        return licensePlateNumber;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
