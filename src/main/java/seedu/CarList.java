c0b3990ce3e44363a8fd9b23b7d03ebc2e913b30
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-15 02:47:37 +0800

    Add new methods in "CarList" class
    getCarsList() -> returns the current car list.
    
    addCar(Car car) -> adds a new Car object to the list.

--- src/main/java/seedu/CarList.java
+++ src/main/java/seedu/CarList.java
@@ -4,4 +4,17 @@ import java.util.ArrayList;
 
 public class CarList {
 
+    public static ArrayList<Car> carsList = new ArrayList<>();
+
+    public static ArrayList<Car> getCarsList() {
+        return carsList;
+    }
+
+    public static void addCar(Car car) {
+        carsList.add(car);
+        System.out.println("Car added to list");
+        System.out.println("Car details:");
+        System.out.println(car.getModel() + " | " + car.getLicensePlateNumber()
+                + " | $" + car.getPrice());
+    }
 }
