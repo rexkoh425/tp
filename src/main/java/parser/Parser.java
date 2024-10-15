eb547b201e5f268fa9b7c406c6e1cd54a7d72e90
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-15 15:25:18 +0800

    Create new "Parser" class to parse user input

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,4 +1,27 @@
 package parser;
 
+import car.Car;
+import car.CarList;
+
 public class Parser {
+
+    public static boolean parse(String userInput) {
+        String[] words = userInput.split(" ");
+        String command = words[0].toLowerCase();
+
+        switch (command) {
+        case "add-car":
+            Car car = CarParser.parseIntoCar(userInput);
+            CarList.addCar(car);
+            return false;
+        case "print":
+            CarList.printCarList();
+            return false;
+        case "exit":
+            return true;
+        default:
+            System.out.println("Invalid command");
+            return false;
+        }
+    }
 }
