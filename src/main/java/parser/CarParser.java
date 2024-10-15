fae4e2a8e9d27664ffe9b321142f61507e01cda6
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-15 15:26:33 +0800

    Add method in "CarParser" class to check validity of format specified by user

--- src/main/java/parser/CarParser.java
+++ src/main/java/parser/CarParser.java
@@ -1,4 +1,19 @@
 package parser;
 
 public class CarParser {
+    public static boolean isValidFormat(String userInput) {
+        for (String param : ADD_CAR_PARAMETERS) {
+            if (!userInput.contains(param)) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < ADD_CAR_PARAMETERS.length - 1; i++) {
+            if (userInput.indexOf(ADD_CAR_PARAMETERS[i]) > userInput.indexOf(ADD_CAR_PARAMETERS[i+1])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
