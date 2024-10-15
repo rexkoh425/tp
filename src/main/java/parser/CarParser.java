3e06f61d2867ebdfd30e92b11383603f3a0c07ac
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-15 15:27:19 +0800

    Add method in "CarParser" class to extract car model from valid user input

--- src/main/java/parser/CarParser.java
+++ src/main/java/parser/CarParser.java
@@ -1,6 +1,17 @@
 package parser;
 
 public class CarParser {
+    private static String extractCarModel(String userInput) {
+        int startIndexOfCarModel = userInput.indexOf(ADD_CAR_PARAMETERS[0]) + ADD_CAR_PARAMETERS_OFFSET;
+        int endIndexOfCarModel = userInput.indexOf(ADD_CAR_PARAMETERS[1]);
+
+        String carModel = userInput.substring(startIndexOfCarModel, endIndexOfCarModel);
+        if (carModel.trim().isEmpty()) {
+            throw new CarException("Car model missing!!");
+        }
+
+        return carModel;
+    }
     public static boolean isValidFormat(String userInput) {
         for (String param : ADD_CAR_PARAMETERS) {
             if (!userInput.contains(param)) {
