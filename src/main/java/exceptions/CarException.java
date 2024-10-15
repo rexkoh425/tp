1f31f72f1cfbae09f43a9f1a84737e124b0f9d04
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-15 15:23:22 +0800

    Add new exception method in "CarException" class

--- src/main/java/exceptions/CarException.java
+++ src/main/java/exceptions/CarException.java
@@ -1,4 +1,17 @@
 package exceptions;
 
-public class CarException {
+public class CarException extends RuntimeException{
+
+    private static final String ADD_CAR_FORMAT = "add-car /n [CAR_MODEL] /c [CAR_ID] /p [PRICE]";
+
+    public CarException(String message) {
+        super(message);
+    }
+
+    public static CarException addCarException() {
+        String message = "Unable to add car.. Refer to correct format below:\n" +
+                ADD_CAR_FORMAT;
+        return new CarException(message);
+    }
+
 }
