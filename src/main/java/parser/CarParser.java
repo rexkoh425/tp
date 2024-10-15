967c47739dc5381b61c7bb9a60b52d75d4ef538a
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-15 15:28:04 +0800

    Add method in "CarParser" class to extract car license plate number from valid user input

--- src/main/java/parser/CarParser.java
+++ src/main/java/parser/CarParser.java
@@ -12,6 +12,21 @@ public class CarParser {
 
         return carModel;
     }
+
+    private static String extractCarLicensePlateNumber(String userInput) {
+        int startIndexOfLicensePlateNumber = userInput.indexOf(ADD_CAR_PARAMETERS[1])
+                + ADD_CAR_PARAMETERS_OFFSET;
+        int endIndexOfLicensePlateNumber = userInput.indexOf(ADD_CAR_PARAMETERS[2]);
+
+        String carLicensePlateNumber = userInput.substring(startIndexOfLicensePlateNumber,
+                endIndexOfLicensePlateNumber);
+        if (carLicensePlateNumber.trim().isEmpty()) {
+            throw new CarException("License plate number missing!!");
+        }
+
+        return carLicensePlateNumber;
+    }
+
     public static boolean isValidFormat(String userInput) {
         for (String param : ADD_CAR_PARAMETERS) {
             if (!userInput.contains(param)) {
