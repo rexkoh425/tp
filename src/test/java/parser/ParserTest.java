563e2654abcfc67aa1ab151a87c2575a184ed7af
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-10-16 21:21:49 +0800

    Update JUnit test methods in ParserTest class

--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -1,27 +1,33 @@
 package parser;
 
-import customer.Customer;
+import car.CarList;
+import customer.CustomerList;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ParserTest {
 
     @Test
-    void parseParameterContents() {
-        String userInput = "add-user /u test /a 18 /c 92750174";
-        String[] parameters = { "/u" , "/a" , "/c"};
-        String[] contents = CustomerParser.parseParameterContents(parameters, userInput);
-        assert contents[0].equals("test");
-        assert contents[1].equals("18");
-        assert contents[2].equals("92750174");
-    }
+    public void parse_validUserInput_commandActionExecuted() {
+        String userInput = "add-user /u user1234 /a 18 /c 92750174";
 
-    @Test
-    void parseIntoCustomer() {
-        String userInput = "add-user /u test /a 18 /c 92750174";
-        Customer customer = CustomerParser.parseIntoCustomer(userInput);
-        assertEquals("test" , customer.getUsername());
-        assertEquals(18 , customer.getAge());
-        assertEquals( 92750174 , customer.getContactNumber());
+        Parser.parse(userInput);
+
+        assertEquals(1, CustomerList.getCustomers().size());
+        assertEquals("user1234", CustomerList.getCustomers().get(0).getUsername());
+        assertEquals(false, Parser.parse(userInput));
+
+        String userInput1 = "add-car /n Honda Civic /c SGE1234T /p 123";
+
+        Parser.parse(userInput1);
+
+        assertEquals(1, CarList.getCarsList().size());
+        assertEquals(123.0, CarList.getCarsList().get(0).getPrice());
+        assertEquals(false, Parser.parse(userInput1));
+
+
+        String userInput2 = "exit";
+        assertEquals(true, Parser.parse(userInput2));
     }
 }
