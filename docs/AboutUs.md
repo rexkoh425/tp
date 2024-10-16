5807581e1fd40514da6b3388004db6ae239d5780
Aaron Liu <94769848+AaronZZ10@users.noreply.github.com>
2024-10-16 17:02:16 +0800

    Revert "A-ListAllCustomers"

--- docs/AboutUs.md
+++ docs/AboutUs.md
@@ -13,6 +13,3 @@ Display | Name | Github Profile | Portfolio
 --------|:----:|:--------------:|:---------:
 ![](https://via.placeholder.com/100.png?text=Photo) | Ryan Fong | [Github](https://github.com/CT9ARyan) | [Portfolio](./team/ryanfong.md)
 
-Display |  Name   |             Github Profile             | Portfolio 
---------|:-------:|:--------------------------------------:|:---------:
-![](https://avatars.githubusercontent.com/u/94769848?v=4) | Liu Hao | [Github](https://github.com/AaronZZ10) | [Portfolio](./team/liuhao.md)
--- src/main/java/customer/CustomerList.java
+++ src/main/java/customer/CustomerList.java
@@ -12,25 +12,8 @@ public class CustomerList {
         System.out.println(customer.toString());
     }
 
-    public static boolean removeCustomer(String Username){
-        for(Customer customer : customers){
-            if(customer.getUsername().equals(Username)){
-                customers.remove(customer);
-                return true;
-            }
-        }
-        return false;
-    }
-
     public static ArrayList<Customer> getCustomers() {
         return customers;
-    }
-
-    public static void printCustomers() {
-        for (Customer customer : customers) {
-            System.out.println(customer.toString());
-            System.out.println("____________________________________________________________");
-        }
     }
 }
 
