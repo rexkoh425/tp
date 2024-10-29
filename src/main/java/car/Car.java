6680a5d4ece3e97a5650d312243d15e299ae6894
tkhahns <111437863+tkhahns@users.noreply.github.com>
2024-10-29 15:53:29 +0800

    Add median calculation feature

--- src/main/java/car/Car.java
+++ src/main/java/car/Car.java
@@ -8,12 +8,14 @@ public class Car {
     private String licensePlateNumber;
     private double price;
     private boolean isRented;
+    private boolean isExpensive;
 
     public Car(String model, String licensePlateNumber, double price) throws CarException{
         this.model = model;
         this.licensePlateNumber = licensePlateNumber;
         this.price = price;
         isRented = false;
+        isExpensive = false;
     }
 
     public String getModel() {
@@ -36,8 +38,23 @@ public class Car {
         isRented = false;
     }
 
+    public void markAsExpensive() { isExpensive = true; }
+
+    public void markAsCheap() { isExpensive = false; }
+
     public boolean isRented() {
         return isRented;
+    }
+
+    public boolean isExpensive() { return isExpensive; }
+
+    public String getExpensiveStatus(){
+        if (isExpensive) {
+            return "Expensive";
+        }
+        else {
+            return "Cheap";
+        }
     }
 
     public String getRentedStatus(){
