f1f6e890702c84a2c55ffab91ea99f834d9b0cce
Aaron Liu <aaronzzz10@foxmail.com>
2024-10-30 06:57:26 +0800

    fix format

--- src/test/java/transaction/TransactionTest.java
+++ src/test/java/transaction/TransactionTest.java
@@ -3,8 +3,9 @@ package transaction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TransactionTest {
 
