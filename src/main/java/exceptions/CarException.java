9c2a3e64583ac48ab59770842f09463a6d6ff101
CT9ARyan <ryan.fong11@yahoo.com.sg>
2024-11-07 11:55:08 +0800

    Edit 'ADD_CAR_FORMAT' message

--- src/main/java/exceptions/CarException.java
+++ src/main/java/exceptions/CarException.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class CarException extends RuntimeException{
 
-    private static final String ADD_CAR_FORMAT = "add-car /n [CAR_MODEL] /c [CAR_ID] /p [PRICE]";
+    private static final String ADD_CAR_FORMAT = "add-car /n [CAR_MODEL] /c [LICENSE_PLATE_NUMBER] /p [PRICE]";
     private static final String LICENSE_PLATE_NUMBER_FORMAT = """
 
             License Plate number format: SXX####X\
