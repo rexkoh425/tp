1b20e5f9598ba5102c00c828b3a60af7df151b26
SemiColonKen <e0811519@u.nus.edu>
2024-11-11 14:17:01 +0800

    Fixed junit test error (TransactionTest)

--- src/test/java/transaction/TransactionTest.java
+++ src/test/java/transaction/TransactionTest.java
@@ -13,6 +13,11 @@ import static parser.TransactionParser.dateTimeFormatter;
 class TransactionTest {
     private Transaction transaction1;
     private Transaction transaction2;
+    private int transactionCounter = 1;
+
+    private String generateTransactionId() {
+        return "TX" + transactionCounter++;
+    }
 
     @BeforeEach
     void setUp() {
@@ -20,9 +25,9 @@ class TransactionTest {
         LocalDate startDate1 = LocalDate.parse("01-10-2024", dateTimeFormatter);
         LocalDate startDate2 = LocalDate.parse("02-10-2024", dateTimeFormatter);
         transaction1 = new Transaction("ABC-123", "John Doe", 5, startDate1);
-        transaction1.setTransactionId(Transaction.generateTransactionId());
+        transaction1.setTransactionId(generateTransactionId());
         transaction2 = new Transaction("XYZ-789", "Jane Smith", 3, startDate2);
-        transaction2.setTransactionId(Transaction.generateTransactionId());
+        transaction2.setTransactionId(generateTransactionId());
     }
 
     @Test
@@ -97,9 +102,9 @@ class TransactionTest {
         LocalDate startDate3 = LocalDate.parse("03-10-2024", dateTimeFormatter);
         LocalDate startDate4 = LocalDate.parse("04-10-2024", dateTimeFormatter);
         Transaction transaction3 = new Transaction("LMN-456", "Alice Johnson", 2, startDate3);
-        transaction3.setTransactionId(Transaction.generateTransactionId());
+        transaction3.setTransactionId(generateTransactionId());
         Transaction transaction4 = new Transaction("DEF-321", "Bob Brown", 4, startDate4);
-        transaction4.setTransactionId(Transaction.generateTransactionId());
+        transaction4.setTransactionId(generateTransactionId());
 
         assertEquals("TX16", transaction3.getTransactionId(), "Third transaction ID should be TX3");
         assertEquals("TX17", transaction4.getTransactionId(), "Fourth transaction ID should be TX4");
