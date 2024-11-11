3d6fc26c8392465632d3ccbce79865b8714c6539
tkhahns <111437863+tkhahns@users.noreply.github.com>
2024-11-11 15:53:33 +0800

    Comment out the clear method before tests

--- src/test/java/transaction/TransactionTest.java
+++ src/test/java/transaction/TransactionTest.java
@@ -1,6 +1,6 @@
 package transaction;
 
-import file.FileHandler;
+//import file.FileHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,8 +22,8 @@ class TransactionTest {
 
     @BeforeEach
     void setUp() {
-        TransactionList.clearTransactionList();
-        FileHandler.createFolderIfNotExist();
+//        TransactionList.clearTransactionList();
+//        FileHandler.createFolderIfNotExist();
         // Initialize two Transaction instances before each test
         LocalDate startDate1 = LocalDate.parse("01-10-2024", dateTimeFormatter);
         LocalDate startDate2 = LocalDate.parse("02-10-2024", dateTimeFormatter);
