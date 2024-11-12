b203ed3fe0d80e0bdb4b929aea64daa925b345c2
SemiColonKen <e0811519@u.nus.edu>
2024-11-12 10:43:55 +0800

    updated remove-user for empty list

--- src/main/java/customer/CustomerList.java
+++ src/main/java/customer/CustomerList.java
@@ -51,16 +51,28 @@ public class CustomerList {
     }
 
     public static void removeCustomer(String customerName) {
-        for (Customer customer : customers) {
+        if (customers.isEmpty()) {
+            System.out.println("Customer list is empty. Nothing to remove");
+            return;
+        }
+
+        boolean customerFound = false;
+
+        for (int i = 0; i < customers.size(); i++) {
+            Customer customer = customers.get(i);
             if (customer.getCustomerName().equalsIgnoreCase(customerName)) {
-                customers.remove(customer);
-                System.out.println("Customer Name: " + customer.getCustomerName() +
-                        " has been removed from the customer list");
-                return;
+                customers.remove(i);
+                System.out.println("Customer Name: " + customer.getCustomerName() + " has been removed from the customer list");
+                customerFound = true;
+                break;
             }
         }
-        System.out.println(customerName + " is not in the customer list. No removal done");
+
+        if (!customerFound) {
+            System.out.println(customerName + " is not in the customer list. No removal done");
+        }
     }
+
 
     public static void clearCustomerList() {
         customers.clear();
